8d94da3d0bd49a2c5ace61f7e5f2c4fe24550313
Crow <devcrowmaster@gmail.com>
2020-05-19 16:46:26 -0500

    Updates

---
+++ precache-manifest.48742ce828521f3532692ddeb2c6db19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ed3b7eeb458ac0d29f0f7b26df16f9d",
+    "url": "/lyrics-app/index.html"
+  },
+  {
+    "revision": "0f41804635d88bedc9bc",
+    "url": "/lyrics-app/static/css/main.8ba7a252.chunk.css"
+  },
+  {
+    "revision": "311eb2804a3551ceea85",
+    "url": "/lyrics-app/static/js/2.52510209.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lyrics-app/static/js/2.52510209.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f41804635d88bedc9bc",
+    "url": "/lyrics-app/static/js/main.9d715729.chunk.js"
+  },
+  {
+    "revision": "1cf216878360490e7450",
+    "url": "/lyrics-app/static/js/runtime-main.73fe7e85.js"
+  }
+]);
